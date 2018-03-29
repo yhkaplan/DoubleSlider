226594ba163cb3c25384cd575c8a1fc34fae25a2
yhkaplan <yhkaplan@gmail.com>
2018-03-29 17:32:21 +0900

    Started adding labels

--- CustomControl.playground/Contents.swift
+++ CustomControl.playground/Contents.swift
@@ -11,6 +11,7 @@ class MyViewController : UIViewController {
 
         let frame = CGRect(x: 10, y: 50, width: 350, height: 32)
         let control = MultiSlider(frame: frame)
+        control.stepDistance = control.frame.width / 10.0
         
         control.addTarget(self, action: #selector(printVal(_:)), for: .valueChanged)
         
@@ -28,62 +29,76 @@ class MyViewController : UIViewController {
     }
 }
 
+// The Color enum is populated with the default color values that Apple uses in
+// standard UIKit controls
 public enum Colors {
     static let defaultBlue = UIColor(red: 0.0, green: 0.51, blue: 0.98, alpha: 1.0)
     static let defaultGray = UIColor(red: 0.71, green: 0.71, blue: 0.71, alpha: 1.0)
     static let defaultWhite = UIColor.white
 }
 
+// The MultiSlider class is the main class that implements the MultiSlider
+// control. Its main methods are marked open, so they can be subclassed and
+// overridden to add or change behaviors
 open class MultiSlider: UIControl {
     
     // Track values
-    var minValue: Double = 0.0 {
+    public var minValue: Double = 0.0 {
         didSet {
             updateLayerFrames()
         }
     }
     
-    var maxValue: Double = 1.0 {
+    public var maxValue: Double = 1.0 {
         didSet {
             updateLayerFrames()
         }
     }
     
-    open var lowerValue: Double = 0.2 {
+    public var lowerValue: Double = 0.2 {
         didSet {
             updateLayerFrames()
         }
     }
     
-    open var upperValue: Double = 0.8 {
+    public var upperValue: Double = 0.8 {
         didSet {
             updateLayerFrames()
         }
     }
     
-    open var stepDistance: CGFloat? = nil
+    // When stepDistance is set to nil, then steps are deactivated
+    public var stepDistance: CGFloat? = nil //TODO: determine if property observer is needed
+    
+    // Override this to show only one label, etc.
+    open var labelsAreHidden: Bool = false {
+        didSet {
+            minLabel.isHidden = labelsAreHidden
+            maxLabel.isHidden = labelsAreHidden
+        }
+    }
     
     // Colors
-    var trackTintColor: UIColor = Colors.defaultGray {
+    public var trackTintColor: UIColor = Colors.defaultGray {
         didSet {
             trackLayer.setNeedsDisplay()
         }
     }
     
-    var trackHighlightTintColor: UIColor = Colors.defaultBlue {
+    public var trackHighlightTintColor: UIColor = Colors.defaultBlue {
         didSet {
             trackLayer.setNeedsDisplay()
         }
     }
     
-    var thumbTintColor: UIColor = Colors.defaultWhite {
+    public var thumbTintColor: UIColor = Colors.defaultWhite {
         didSet {
             lowerThumbLayer.setNeedsDisplay()
             upperThumbLayer.setNeedsDisplay()
         }
     }
     
-    var roundedness: CGFloat = 1.0 {
+    public var roundedness: CGFloat = 1.0 {
         didSet {
             trackLayer.setNeedsDisplay()
             lowerThumbLayer.setNeedsDisplay()
@@ -92,9 +107,11 @@ open class MultiSlider: UIControl {
     }
     
     // Render components
-    let trackLayer = MultiSliderTrackLayer()
-    let lowerThumbLayer = MultiSliderThumbLayer()
-    let upperThumbLayer = MultiSliderThumbLayer()
+    public let trackLayer = MultiSliderTrackLayer()
+    public let lowerThumbLayer = MultiSliderThumbLayer()
+    public let upperThumbLayer = MultiSliderThumbLayer()
+    public let minLabel = MultiSliderTextLayer()
+    public let maxLabel = MultiSliderTextLayer()
     
     var previousLocation = CGPoint()
     
@@ -102,7 +119,7 @@ open class MultiSlider: UIControl {
         return CGFloat(bounds.height)
     }
     
-    override var frame: CGRect {
+    override open var frame: CGRect {
         didSet {
             updateLayerFrames()
         }
@@ -123,6 +140,14 @@ open class MultiSlider: UIControl {
         upperThumbLayer.contentsScale = UIScreen.main.scale
         layer.addSublayer(upperThumbLayer)
         
+        minLabel.multiSlider = self
+        minLabel.contentsScale = UIScreen.main.scale
+        layer.addSublayer(minLabel)
+        
+        maxLabel.multiSlider = self
+        maxLabel.contentsScale = UIScreen.main.scale
+        layer.addSublayer(maxLabel)
+        
         updateLayerFrames()
     }
     
@@ -130,7 +155,7 @@ open class MultiSlider: UIControl {
         super.init(coder: aDecoder)
     }
     
-    private func updateLayerFrames() {
+    open func updateLayerFrames() {
         CATransaction.begin()
         CATransaction.setDisableActions(true) // Prevents interaction while updating
         
@@ -138,13 +163,25 @@ open class MultiSlider: UIControl {
         trackLayer.setNeedsDisplay()
         
         //TODO: simplify below into helper methods / closures
+        // Update lower values
         let lowerThumbCenter = CGFloat(positionForValue(value: lowerValue))
-        let lowerX = lowerThumbCenter - thumbWidth / 2.0
+        var lowerX = lowerThumbCenter - thumbWidth / 2.0
+        
+        if let stepDistance = stepDistance, stepDistance > 0.0 { //TODO: make this extension or helper method
+            lowerX = CGFloat(roundf(Float(lowerX / stepDistance))) * stepDistance
+        }
+            
         lowerThumbLayer.frame = CGRect(x: lowerX, y: 0.0, width: thumbWidth, height: thumbWidth)
         lowerThumbLayer.setNeedsDisplay()
         
+        // Update upper values
         let upperThumbCenter = CGFloat(positionForValue(value: upperValue))
-        let upperX = upperThumbCenter - thumbWidth / 2.0
+        var upperX = upperThumbCenter - thumbWidth / 2.0
+        
+        if let stepDistance = stepDistance, stepDistance > 0.0 {
+            upperX = CGFloat(roundf(Float(upperX / stepDistance))) * stepDistance
+        }
+        
         upperThumbLayer.frame = CGRect(x: upperX, y: 0.0, width: thumbWidth, height: thumbWidth)
         upperThumbLayer.setNeedsDisplay()
         
@@ -207,7 +244,14 @@ open class MultiSlider: UIControl {
     }
 }
 
-open class MultiSliderThumbLayer: CALayer {
+// MultiSliderThumbLayer represents the each of the draggable
+// thumb layers that indicate the minimum and maximum values
+// in the range
+// I decided not to make this class open because it is very small,
+// meaning that developers could make their own custom CALayer
+// subclass in the same amount of time as it would take to subclass
+// this one
+public class MultiSliderThumbLayer: CALayer {
     weak var multiSlider: MultiSlider?
     
     var isHighlighted: Bool = false {
@@ -216,7 +260,7 @@ open class MultiSliderThumbLayer: CALayer {
         }
     }
     
-    override open func draw(in ctx: CGContext) {
+    override public func draw(in ctx: CGContext) {
         guard let slider = multiSlider else { return }
         
         let insetSize: CGFloat = 2.0
@@ -246,10 +290,16 @@ open class MultiSliderThumbLayer: CALayer {
     }
 }
 
-open class MultiSliderTrackLayer: CALayer {
+// MultiSliderTrackLayer represents the colorized space between
+// the two thumb layers
+// I decided not to make this class open because it is very small,
+// meaning that developers could make their own custom CALayer
+// subclass in the same amount of time as it would take to subclass
+// this one
+public class MultiSliderTrackLayer: CALayer {
     weak var multiSlider: MultiSlider?
     
-    override open func draw(in ctx: CGContext) {
+    override public func draw(in ctx: CGContext) {
         guard let slider = multiSlider else { return }
         
         // Clip
@@ -273,6 +323,10 @@ open class MultiSliderTrackLayer: CALayer {
                           height: bounds.height)
         ctx.fill(rect)
     }
+}
+
+public class MultiSliderTextLayer: CATextLayer {
+    weak var multiSlider: MultiSlider?
 }
 
 // Present the view controller in the Live View window
