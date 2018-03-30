d3e729962070b953d883ea4cc4aedcf187ad72f0
yhkaplan <yhkaplan@gmail.com>
2018-03-30 13:13:32 +0900

    Got basic label functionality working

--- MultiSlider/MultiSlider/CALayers/MultiSliderTextLayer.swift
+++ MultiSlider/MultiSlider/CALayers/MultiSliderTextLayer.swift
@@ -10,4 +10,16 @@ import QuartzCore
 
 public class MultiSliderTextLayer: CATextLayer, MultiSliderLayer {
     weak var multiSlider: MultiSlider?
+    
+//    // For adding label background
+//    public override func draw(in ctx: CGContext) {
+//        guard let slider = multiSlider else { return }
+//
+//        let cornerRadius = bounds.height * slider.roundedness / 2.0
+//        let path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+//
+//        ctx.setFillColor(UIColor.black.cgColor) //TODO: make color customizable
+//        ctx.addPath(path.cgPath)
+//        ctx.fillPath()
+//    }
 }
--- MultiSlider/MultiSlider/MultiSliderClass/MultiSliderUpdateFunctions.swift
+++ MultiSlider/MultiSlider/MultiSliderClass/MultiSliderUpdateFunctions.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+// MARK: General update funcs
+
 extension MultiSlider {
     
     open func updateLayerFrames() {
@@ -42,7 +44,12 @@ extension MultiSlider {
         
         updateLabelPositions()
         updateLabelValues()
-        
+        minLabel.alignmentMode = kCAAlignmentCenter
+//        minLabel.display()
+//        minLabel.setNeedsDisplay()
+        maxLabel.alignmentMode = kCAAlignmentCenter
+//        maxLabel.display()
+//        maxLabel.setNeedsDisplay()
         CATransaction.commit()
     }
     
@@ -51,21 +58,39 @@ extension MultiSlider {
         let widthDifference = Double(bounds.width - thumbWidth)
         return widthDifference * (value - minValue) / (maxValue - minValue) + Double(thumbWidth / 2.0)
     }
-    // MARK: Label related funcs TODO: move to extension in separate file
+}
+
+// MARK: Label related funcs
+
+extension MultiSlider {
+    
     private func updateLabelValues() {
-        minLabel.string = NumberFormatter().string(from: minValue as NSNumber)
-        maxLabel.string = NumberFormatter().string(from: maxValue as NSNumber)
+        let numberFormatter = NumberFormatter() //TODO: move to extension
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal//TODO: needed?
+
+        guard let rawMinString = numberFormatter.string(from: lowerValue as NSNumber),
+            let rawMaxString = numberFormatter.string(from: upperValue as NSNumber) else {
+                return
+        }
+
+        let attributes: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 12.0),
+            NSAttributedStringKey.foregroundColor: UIColor.black.cgColor
+        ]
+        
+        minLabel.string = NSAttributedString(string: rawMinString, attributes: attributes)
+        maxLabel.string = NSAttributedString(string: rawMaxString, attributes: attributes)
         
         setNeedsLayout()
     }
     
     private func updateLabelPositions() {
-        let labelSize = CGSize(width: 16, height: 8)
+        let labelSize = CGSize(width: 40, height: 20)
         minLabel.frame.size = labelSize
         maxLabel.frame.size = labelSize
         
         let minimumSpaceBetweenLabels: CGFloat = 8.0
-        let spaceBetweenThumbAndLabel: CGFloat = 8.0
+        let spaceBetweenThumbAndLabel: CGFloat = 20.0
         
         let newMinY = lowerThumbLayer.frame.midY - (minLabel.frame.height / 2.0) - spaceBetweenThumbAndLabel
         let newMinLabelCenter = CGPoint(x: lowerThumbLayer.frame.midX, y: newMinY)
@@ -91,4 +116,3 @@ extension MultiSlider {
         } else {} //TODO: see585
     }
 }
-
